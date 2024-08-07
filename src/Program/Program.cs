6d3b65503961388a4518ba8e2bfb713a97cdb807
Thiago202005 <thiago.garcia@correo.ucu.edu.uy>
2024-08-07 10:36:48 -0300

    implementacion de la operacion division

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
-Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
+﻿Console.WriteLine(Division.Divide(18, 2));
+
+public class Division
+{
+    public static int Divide(int a, int b)
+    {
+        return a / b;
+    }
+}
