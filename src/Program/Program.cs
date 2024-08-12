dddbef3d5821a68540b99ef5f9d877176ae4c95f
luis.vazquez@correo.ucu.edu.uy <luis.vazquez@correo.ucu.edu.uy>
2024-08-12 09:05:36 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿// Esta clase implementa la operación division
 Console.WriteLine(Division.Divide(18, 2));
 
@@ -8,3 +9,18 @@ public class Division
         return a / b;
     }
 }
+=======
+﻿// Esta clase implementa la operación <nombre-operación>
+Console.WriteLine(Subtraction.Substract(10, 1));
+
+public class Subtraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+    
+    
+}
+
+>>>>>>> feature-subtraction3
